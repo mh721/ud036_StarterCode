302e94dd84160a0eb05774592670690523d18e68
Martin H <martin.handzus@gmail.com>
2018-10-24 22:10:28 +0200

    [UPDATE] building list of movies

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,12 +1,26 @@
 import media
 import fresh_tomatoes
 
-toy_story = media.Movie("Toy Story", "A story of a boy and his toys that come to life",
-                    "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
-                    "https://www.youtube.com/watch?v=vwyZH85NQC4")
+# initializing the movies
 
-# toy_story.show_trailer()
+riddick = media.Movie("Riddick", 
+                      "Left for dead on a sun-scorched planet, Riddick " +
+                      "finds himself up against an alien race of " +
+                      "predators. Activating an emergency beacon alerts " +
+                      "two ships: one carrying a new breed of mercenary, " +
+                      "the other captained by a man from Riddick's past.",
+                      "https://images-na.ssl-images-amazon.com/images/I/71J21ePe%2BsL._SY445_.jpg",
+                      "https://www.youtube.com/watch?v=iP3eFIOBU0k"
+                      )
 
-movies = [toy_story]
+deadpool = media.Movie("Deadpool", 
+                       "A fast-talking mercenary with a morbid sense of humor " +
+                       "is subjected to a rogue experiment that leaves him " +
+                       "with accelerated healing powers and a quest for revenge.",
+                       "https://upload.wikimedia.org/wikipedia/en/2/23/Deadpool_%282016_poster%29.png",
+                       "https://www.youtube.com/watch?v=ONHBaC-pfsk"
+                       )
+
+movies = [riddick, deadpool] 
 
 fresh_tomatoes.open_movies_page(movies)
